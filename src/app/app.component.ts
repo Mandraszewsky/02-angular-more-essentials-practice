93edba35cdcd7d765752aa48d8b5df257ba7b5d6
Mandraszewsky <mandrah@o2.pl>
2024-11-04 12:14:18 +0100

    passing data from paret to child (also outputting & formatting data)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,27 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
 import { InvestmentInput } from "./investment-input.model"
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-  onCalculateInvestmentResults(data: InvestmentInput) {
 
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
     const annualData = [];
     const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
 
@@ -34,6 +45,6 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
+    this.resultsData = annualData;
   }
 }
